1f5a7436361704dedbc53bf77b968b46b10c1589
tekthiyagu <thiyaagugeek@gmail.com>
2022-08-20 16:25:35 +0530

    1 file modified

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>Object Tutor</h1>`;
+
+console.log('object constructor function');
